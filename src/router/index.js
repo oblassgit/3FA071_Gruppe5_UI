525fc89773df180aa5d9c82cbf1cc1c74848da57
Oliver Blass <oliverblass@web.de>
2025-03-13 12:57:08 +0100

    fixed everything

--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,20 @@ import { createRouter, createWebHistory } from "vue-router";
 import CustomerList from "@/components/CustomerList.vue";
 import CustomerForm from "@/components/CustomerForm.vue";
 import CustomerDetail from "@/components/CustomerDetail.vue";
+import ReadingList from "@/components/ReadingList.vue";
+import ReadingForm from "@/components/ReadingForm.vue";
+import ReadingDetail from "@/components/ReadingDetail.vue";
 
 const routes = [
     { path: "/", component: CustomerList },
     { path: "/customers/new", component: CustomerForm },
-    { path: "/customers/:id", component: CustomerDetail, props: true },
-    { path: "/customers/edit/:id", component: CustomerForm, props: true }
+    { path: "/customers", component: CustomerList },
+    { path: "/customers/:id", component: CustomerDetail, props: true, name: "customerDetail" },
+    { path: "/customers/edit/:id", component: CustomerForm, props: true },
+    { path: "/readings", component: ReadingList },
+    { path: "/readings/new", component: ReadingForm },
+    { path: "/readings/:id", component: ReadingDetail, props: true },
+    { path: "/readings/edit/:id", component: ReadingForm, props: true },
 ];
 
 const router = createRouter({
