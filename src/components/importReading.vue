094698682d05d9cc4b3ed69096ddb86b106125d9
David-Zorn <dave_king@outlook.de>
2025-03-21 10:45:51 +0100

    wip

--- src/components/importReading.vue
+++ src/components/importReading.vue
@@ -25,7 +25,7 @@
 import Papa from 'papaparse';
 import axios from 'axios';
 
-const url = 'http://127.0.0.1:8080/rest';
+const url = 'http://127.0.0.1:8080/rest/customers/createcustomers';
 
 export default {
   data() {
